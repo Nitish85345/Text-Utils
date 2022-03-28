5a85ae2a2d07e4490eaf1f3b6ea853bb6e2a9442
Nitish <nitishkumar0394@gmail.com>
2022-03-28 12:40:35 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam distinctio hic pariatur in illum optio expedita exercitationem natus, incidunt nesciunt, id ea voluptate ullam, harum fugiat? Voluptas eaque facilis voluptatem!
+    </div>
+    </>
+  );
+}
+
+export default App;
