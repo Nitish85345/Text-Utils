a0a776180ccf5d097747a0641be85f629f64387f
Nitish <nitishkumar0394@gmail.com>
2022-03-29 15:50:34 +0530

    Added Router and Accordian in About section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
+import About from './Components/About';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Textform title="Enter Text Here"/>
-    </>
+    <Router>
+      <div>
+        <Navbar />
+
+        <Routes>
+          <Route path="/"
+            element={<Textform title="Enter Text Here" />}
+          />
+
+          <Route path="/about"
+            element={<About />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
